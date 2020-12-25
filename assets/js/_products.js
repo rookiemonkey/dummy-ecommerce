b8d4df0e12f674ebef5a712c2b9096aa5c4612a6
rookiemonkey <kevinroirigorbasina@pm.me>
2020-12-25 09:10:32 +0800

    add preloader

--- assets/js/_products.js
+++ assets/js/_products.js
@@ -9,12 +9,14 @@ const baseurl = 'http://localhost:5050';
     const header_pets = document.getElementById('header-pets');
     const header_fashionwomen = document.getElementById('header-fashionwomen');
     const header_fashionmen = document.getElementById('header-fashionmen');
-    const header_accesories = document.getElementById('header-accessories');
+    const header_accessories = document.getElementById('header-accessories');
     const header_sportsandlifestyle = document.getElementById('header-sportsandlifestyle');
     const header_automotive = document.getElementById('header-automotive');
 
     const raw = await fetch(`${baseurl}/api/v1/departments?apikey=gFKVHZjIK_Wt`);
     const { data } = await raw.json();
+
+    console.log(data)
 
     const appendNumProducts = (element, num) => {
         element.innerHTML = `
@@ -48,8 +50,8 @@ const baseurl = 'http://localhost:5050';
             case dept.department_name === 'Fashion Men':
                 appendNumProducts(header_fashionmen, dept.department_numProducts)
                 break;
-            case dept.department_name === 'Accesories':
-                appendNumProducts(header_accesories, dept.department_numProducts)
+            case dept.department_name === 'Accessories':
+                appendNumProducts(header_accessories, dept.department_numProducts)
                 break;
             case dept.department_name === 'Sports & Lifestyle':
                 appendNumProducts(header_sportsandlifestyle, dept.department_numProducts)
@@ -61,80 +63,99 @@ const baseurl = 'http://localhost:5050';
     })
 })();
 
-(async function renderTopRated() {
+async function renderTopRated() {
     const raw = await fetch(`${baseurl}/api/v1/products/toprated?apikey=gFKVHZjIK_Wt&limit=5`)
     const parsed = await raw.json();
     generanteDom(parsed.data, HTMLProductCard, '#toprated-cards');
-})();
+};
 
-(async function renderTopSales() {
+async function renderTopSales() {
     const raw = await fetch(`${baseurl}/api/v1/products/topsales?apikey=gFKVHZjIK_Wt&limit=3`)
     const parsed = await raw.json();
     generanteDom(parsed.data, HTMLHomeTopSales, '#topsales-cards');
-})();
+};
 
-(async function renderGadgetGrid() {
+async function renderGadgetGrid() {
     const raw = await fetch(`${baseurl}/api/v1/departments/gadgets?apikey=gFKVHZjIK_Wt&limit=4`)
     const parsed = await raw.json();
     generanteDom(parsed.data, HTMLProductCard, '#list-gadgets');
-})();
+};
 
-(async function renderAppliancesGrid() {
+async function renderAppliancesGrid() {
     const raw = await fetch(`${baseurl}/api/v1/departments/appliances?apikey=gFKVHZjIK_Wt&limit=4`)
     const parsed = await raw.json();
     generanteDom(parsed.data, HTMLProductCard, '#list-appliances');
-})();
+};
 
-(async function renderHealthAndBeautyRow() {
+async function renderHealthAndBeautyRow() {
     const raw = await fetch(`${baseurl}/api/v1/departments/healthandbeauty?apikey=gFKVHZjIK_Wt&limit=5`)
     const parsed = await raw.json();
     generanteDom(parsed.data, HTMLProductCard, '#list-healthandbeauty')
-})();
+};
 
-(async function renderBabiesRow() {
+async function renderBabiesRow() {
     const raw = await fetch(`${baseurl}/api/v1/departments/babies?apikey=gFKVHZjIK_Wt&limit=5`)
     const parsed = await raw.json();
     generanteDom(parsed.data, HTMLProductCard, '#list-babies')
-})();
+};
 
-(async function renderGroceriesRow() {
+async function renderGroceriesRow() {
     const raw = await fetch(`${baseurl}/api/v1/departments/groceries?apikey=gFKVHZjIK_Wt&limit=5`)
     const parsed = await raw.json();
     generanteDom(parsed.data, HTMLProductCard, '#list-groceries')
-})();
+};
 
-(async function renderPetsRow() {
+async function renderPetsRow() {
     const raw = await fetch(`${baseurl}/api/v1/departments/pets?apikey=gFKVHZjIK_Wt&limit=5`)
     const parsed = await raw.json();
     generanteDom(parsed.data, HTMLProductCard, '#list-pets')
-})();
+};
 
-(async function renderFashionWomenRow() {
+async function renderFashionWomenRow() {
     const raw = await fetch(`${baseurl}/api/v1/departments/fashionwomen?apikey=gFKVHZjIK_Wt&limit=5`)
     const parsed = await raw.json();
     generanteDom(parsed.data, HTMLProductCard, '#list-fashionwomen')
-})();
+};
 
-(async function renderFashionMenRow() {
+async function renderFashionMenRow() {
     const raw = await fetch(`${baseurl}/api/v1/departments/fashionmen?apikey=gFKVHZjIK_Wt&limit=5`)
     const parsed = await raw.json();
     generanteDom(parsed.data, HTMLProductCard, '#list-fashionmen')
-})();
+};
 
-(async function renderAccesoriesRow() {
+async function renderAccesoriesRow() {
     const raw = await fetch(`${baseurl}/api/v1/departments/accessories?apikey=gFKVHZjIK_Wt&limit=5`)
     const parsed = await raw.json();
     generanteDom(parsed.data, HTMLProductCard, '#list-accessories')
-})();
+};
 
-(async function rendersSportsAndLifestyleRow() {
+async function rendersSportsAndLifestyleRow() {
     const raw = await fetch(`${baseurl}/api/v1/departments/sportsandlifestyle?apikey=gFKVHZjIK_Wt&limit=5`)
     const parsed = await raw.json();
     generanteDom(parsed.data, HTMLProductCard, '#list-sportsandlifestyle')
-})();
+};
 
-(async function rendersAutomotiveRow() {
+async function rendersAutomotiveRow() {
     const raw = await fetch(`${baseurl}/api/v1/departments/automotive?apikey=gFKVHZjIK_Wt&limit=5`)
     const parsed = await raw.json();
     generanteDom(parsed.data, HTMLProductCard, '#list-automotive')
-})();
+};
+
+// Hide the loader once all of the HTTP requests are done
+Promise.all([
+    renderTopRated(), renderTopSales(),
+    renderGadgetGrid(), renderAppliancesGrid(),
+    renderBabiesRow(), renderHealthAndBeautyRow(),
+    renderGroceriesRow(), renderPetsRow(),
+    renderFashionWomenRow(), renderFashionMenRow(),
+    renderAccesoriesRow(), rendersSportsAndLifestyleRow(),
+    rendersAutomotiveRow()
+])
+    .then(() => {
+        const main = document.querySelector('main');
+        const preloader = document.querySelector('.preloader')
+        main.style.display = 'block';
+        preloader.style.opacity = 0;
+        setTimeout(() => preloader.remove(), 500)
+    })
+    .catch(() => alert('Reached Maximum API Call Limit :D'))
