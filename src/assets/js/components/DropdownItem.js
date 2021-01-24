03c3886a1cb9ff8bfd380b5a822e07bb8d3da659
rookiemonkey <kevinroirigorbasina@pm.me>
2021-01-24 20:16:28 +0800

    issue #15: responsiveness NAVBAR! department options working

--- src/assets/js/components/DropdownItem.js
+++ src/assets/js/components/DropdownItem.js
@@ -1,23 +1,35 @@
 import Calzada from '../main';
 import variables from '../utilities/_variables';
+import toToggleSideBar from '../utilities/toToggleSidebar';
 const { navIcons } = variables;
 
 export default function HTMLDropDownItem(department) {
     const { department_id, department_name } = department
-    const nav_dropdown = document.querySelector('.nav-dropdown');
-    const dropdown_item = document.createElement('li');
+    const nav_dropdown_desktop = document.querySelector('.nav-dropdown');
+    const nav_dropdown_mobile = document.querySelector('.nav-dropdown-mobile');
+    const dropdown_item_desktop = document.createElement('li');
 
-    dropdown_item.setAttribute('deptId', department_id);
-    dropdown_item.setAttribute('deptName', department_name);
+    dropdown_item_desktop.setAttribute('deptId', department_id);
+    dropdown_item_desktop.setAttribute('deptName', department_name);
 
-    dropdown_item.innerHTML = `
+    dropdown_item_desktop.innerHTML = `
             <span><i class="${navIcons[department_name]}"></i></span>
             ${department_name}
         `
 
-    // mount an onclick listener
-    dropdown_item.onclick = event => Calzada.toDepartment(event);
+    // clone for mobile, however, events are not included
+    const dropdown_item_mobile = dropdown_item_desktop.cloneNode(true)
 
-    // mount to dom
-    nav_dropdown.appendChild(dropdown_item);
+    // mount an onclick listeners
+    dropdown_item_desktop.onclick = event =>
+        Calzada.toDepartment(event);
+
+    dropdown_item_mobile.onclick = event => {
+        Calzada.toDepartment(event);
+        toToggleSideBar('close');
+    };
+
+    // mount to dom for desktop/mobile
+    nav_dropdown_desktop.appendChild(dropdown_item_desktop);
+    nav_dropdown_mobile.appendChild(dropdown_item_mobile);
 }
